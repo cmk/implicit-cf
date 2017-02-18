f64e49b47d5368c6aaeefde25bf40e0a5598f52c
chris mckinlay <cem3394@chriss-MacBook.att.net>
2017-02-18 12:37:53 -0800

    randomized seeding

--- src/MatrixFactorization.hs
+++ src/MatrixFactorization.hs
@@ -7,8 +7,10 @@ import qualified Data.Map as M
 
 import qualified Numeric.SGD as S
 import qualified Numeric.SGD.Dataset as D
+import Numeric (showFFloat)
 import Data.List (foldl')
 import qualified Bias as B
+import Text.Printf
 
 import Utils
 
@@ -17,6 +19,9 @@ import Debug.Trace
 import qualified Data.Matrix as DM
 
 
+import System.Random (randomRIO)
+import Control.Monad (replicateM)
+
 type Matrices = U.Vector Double -- factor matrices P, Q^t stored in row-major order
 
 
@@ -24,15 +29,16 @@ type Matrices = U.Vector Double -- factor matrices P, Q^t stored in row-major or
 
 user Reader to encode config stuff like Matrices, BiasModel etc
 fix gradP, gradQ repetition with ap or map2 or somesuch
+nItems = 10 --should be max item # found +1
 -}
 
 
-rate = 0.1 -- initial learning rate
-alpha = 0.0 -- confidence scaling factor, see Hu et al section 4
+rate = 0.25 -- initial learning rate
+alpha = 0.2 -- confidence scaling factor, see Hu et al section 4
 lambda = 0.0 -- Tikhonov regularization paramater
 
-nr_iter = 50
-nFeatures = 1
+nr_iter = 150
+nFeatures = 3
 nUsers = 10
 nItems = 10 --should be max item # found +1
 fRange = [0..nFeatures-1]
@@ -64,7 +70,8 @@ getR matrices = let
   in DM.multStd p $ DM.transpose q
 
 showMat :: DM.Matrix Double -> String
-showMat m = DM.prettyMatrix $ fmap (\f -> (fromInteger $ round $ f * (10^2)) / (10.0^^2)) m
+showMat m = show $ fmap (\i -> showFFloat (Just 2) i "") m
+
 
 -- | computes a prediction for the rating of user u on item i 
 predict :: BiasModel -> Matrices -> User -> Item -> Rating
@@ -92,9 +99,11 @@ userIndex x f = nItems * nFeatures  + f + x * nFeatures
 model :: DataSet -> IO Matrices
 model dataset = do
   let n = (nFeatures * nUsers) + (nFeatures * nItems)
-  let matrices = U.replicate n (0.1::Double)
+  matrices <- randomVec n
   D.withVect (V.toList dataset) (runSGD matrices dataset)
 
+randomVec :: Int -> IO Matrices
+randomVec n = fmap U.fromList $ replicateM n $ randomRIO (0,1)
 
 -- | SGD optimizer
 runSGD :: Matrices -> DataSet -> D.Dataset DataPoint -> IO Matrices
@@ -105,7 +114,7 @@ runSGD matrices all d = do
 
 -- notification run by the SGD function 
 notify :: BiasModel -> DataSet -> Matrices -> Int -> IO ()
-notify biasModel all matrices k = putStr "\n"
+notify biasModel all matrices k = putStr ""
 {-("\n" ++
                                           (show k) ++
                                           ("\t") ++
@@ -121,33 +130,35 @@ goal biasModel matrices all = V.sum (V.map (\x -> err2 x) all)
 
 -- gradient descent step
 grad :: BiasModel -> Matrices -> DataPoint -> S.Grad
-grad biasModel matrices dataPoint | trace ("grad: " ++ (show dataPoint)) False = undefined
+--grad biasModel matrices dataPoint | trace ("grad: " ++ (show dataPoint)) False = undefined
 grad biasModel matrices dataPoint = S.fromList gradient
   where gradient = (gradP biasModel matrices dataPoint) ++ (gradQ biasModel matrices dataPoint)           
 
 -- computes the gradient step for the item component
 gradP :: BiasModel -> Matrices -> DataPoint -> [(Int, Double)]  
-gradP biasModel matrices dataPoint = do
-  f <- fRange
-  let (user,item,rating) = dataPoint
-      conf = confidence rating
-      err = errorAt biasModel matrices dataPoint
-      update = conf * err * (matrices U.! (userIndex user f)) - lambda * (matrices U.! (itemIndex item f))
-  return (itemIndex item f, update)
+gradP biasModel matrices dataPoint = let
+  (user,item,rating) = dataPoint
+  conf = confidence rating
+  err = errorAt biasModel matrices dataPoint
+  in do
+    f <- fRange
+    let update = conf * err * (matrices U.! (userIndex user f)) - lambda * (matrices U.! (itemIndex item f))
+    return (itemIndex item f, update)
 
 -- computes the gradient step for the user component
 gradQ :: BiasModel -> Matrices -> DataPoint -> [(Int, Double)]
-gradQ biasModel matrices dataPoint = do
-  f <- fRange
-  let (user,item,rating) = dataPoint
-      conf = confidence rating
-      err = errorAt biasModel matrices dataPoint
-      update = conf * err * (matrices U.! (itemIndex item f)) - lambda * (matrices U.! (userIndex user f))
-  return (userIndex user f, update)
+gradQ biasModel matrices dataPoint = let
+  (user,item,rating) = dataPoint
+  conf = confidence rating
+  err = errorAt biasModel matrices dataPoint
+  in do
+    f <- fRange
+    let update = conf * err * (matrices U.! (itemIndex item f)) - lambda * (matrices U.! (userIndex user f))
+    return (userIndex user f, update)
 
 -- calculates error for user, item, rating with a given latent factor vector
 errorAt :: BiasModel -> Matrices -> DataPoint -> Error 
-errorAt biasModel matrices (user,item,rating) = rating - bias - prediction 
+errorAt biasModel matrices (user,item,rating) = preference rating - bias - prediction 
   where prediction = (sum [(matrices U.! (itemIndex item f)) * (matrices U.! (userIndex user f))| f <- fRange])
         bias = 0.0 --B.predict biasModel user item
 
@@ -157,7 +168,7 @@ confidence :: Rating -> Double
 confidence rating = 1 + alpha * rating
 
 preference :: Rating -> Double
-preference rating | rating > 0.0 = 1.0
+preference rating | rating > 1.0 = 1.0
                   | otherwise = 0.0
 
 {-
