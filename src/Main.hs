8e74450b94ff263018fea7cbd24668de156f77a0
chris mckinlay <cem3394@chriss-MacBook.att.net>
2017-02-17 16:44:45 -0800

    working beta

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,19 @@
-
 module Main where
 
 import Data.Csv
 
-import Bias as B
+
 import System.Environment
 import qualified Data.Vector as V
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.MultiMap as MM
-import qualified Data.Map as M
-import qualified MatrixFactorization as MF
 
-type Rating = (Int, Int, Double)
-type User = Int
-type Item = Int
+--import qualified Data.MultiMap as MM
+--import qualified Data.Map as M
+
+import qualified MatrixFactorization as MF
+import qualified Bias as B
+import Utils
+
 
 main = do
   (command:argList) <- getArgs
@@ -30,16 +30,16 @@ matrixFactorization (base:test:xs) =  do
   (traindata, testdata) <- loadData base test
   m <- MF.model traindata
   let bm = B.model traindata
-  putStrLn $ "Mean Absolute Error: " ++ (show $ mae (MF.predict m bm) testdata)  
+  putStrLn $ "Mean Absolute Error: " ++ (show $ mae (MF.predict bm m) testdata)  
 
-toVec :: Either String (V.Vector Rating) -> V.Vector Rating
+toVec :: Either String DataSet -> DataSet
 toVec (Left err) = error err
 toVec (Right v) = v
 
-mae :: (Int -> Int -> Double) -> V.Vector Rating -> Double
+mae :: (Int -> Int -> Double) -> DataSet -> Double
 mae p v = (V.sum $ errors p v) / (fromIntegral (V.length v))
 
-errors :: ( Int -> Int -> Double ) -> V.Vector Rating -> V.Vector Double
+errors :: ( Int -> Int -> Double ) -> DataSet -> V.Vector Double
 errors p v = V.filter (\x -> not $ isNaN x) $ V.map (\(u, i, r) -> abs (r - p u i)) v
   where diff (u, i, r) = abs (r - p u i)
 
@@ -49,12 +49,84 @@ bias (base:test:xs) = do
   (modelv, testv) <- loadData base test
   putStrLn $ "Mean Absolute Error: " ++ (show $ mae (B.predict $ B.model modelv) testv)  
 
-loadData :: String -> String -> IO (V.Vector Rating, V.Vector Rating) 
+loadData :: String -> String -> IO (DataSet, DataSet) 
 loadData basefile testfile = do
   c1 <- BL.readFile basefile
   c2 <- BL.readFile testfile
-  let baseData = decode NoHeader c1 :: Either String (V.Vector Rating)
-  let testData = decode NoHeader c2 :: Either String (V.Vector Rating)
+  let baseData = decode NoHeader c1 :: Either String (DataSet)
+  let testData = decode NoHeader c2 :: Either String (DataSet)
   let modelv =  toVec baseData
   let testv = toVec testData
   return (modelv, testv)
+
+{-
+
+import Text.CSV (parseCSV, Record)
+import Data.List.Split (splitOn)
+import Text.Read (readMaybe)
+import qualified Data.Map.Strict as M
+
+import Data.Science.CF
+
+
+type Score = Float
+
+-- | The mapping of all items to the user's Score
+type ScoreMap b = (M.Map b Score)
+
+-- | The collection of a user's ratings
+-- | where a is the user identifier type, b is the item identifier type
+data Sample a b = DataPoint a (ScoreMap b) deriving (Show, Eq)
+
+
+type User = Sample Int Int
+
+main :: IO ()
+main = do
+  let fileName = "data/test.csv"
+  input <- readFile fileName
+  let csv = parseCSV fileName input
+  either print doWork csv
+
+doWork :: [Record] -> IO ()
+doWork csv = let
+  users = map avg $ M.elems $ M.fromListWith combine $ map parseToTuple $ tail $ init csv
+  user = avg $ DataPoint (-1) $ M.fromList [(8,2),(48,1)]
+  closests = take 30 $ sortNeighbours cosine user users
+  recs = recommend cosine user users
+  r = ratings $ findUser (snd $ head closests) users
+  in do
+    --print users
+    print closests
+    print recs
+    print user
+    print r
+
+avg :: User -> User
+avg (DataPoint user ratings) = let
+  sum = M.foldl' (+) 0.0 ratings
+  ratings' = M.map (\r -> r/sum) ratings
+  in DataPoint user ratings'
+
+  ,doWork' :: [Record] -> IO ()
+doWork' csv = let
+  csv' = tail $ init csv
+  read' r = maybe (-1) id (readMaybe r :: Maybe Int) 
+  items record = map read' $ splitOn "|" $ head $ tail record
+  in print $ map items csv'
+
+combine :: User -> User -> User
+combine (DataPoint a1 b1) (DataPoint a2 b2) =
+  let new = M.unionWith (+) b1 b2 :: ScoreMap Int
+  in DataPoint a1 new
+
+parseToTuple :: [String] -> (Int,User)
+parseToTuple record = let
+  read' r = maybe (-1) id (readMaybe r :: Maybe Int)
+  --read' r = read r :: Int
+  name = read' $ head record 
+  items = map read' $ splitOn "|" $ head $ tail record
+  items' = M.fromListWith (+) $ map (\k -> (k,1)) items
+  in (name, DataPoint name items')
+
+-}
--- src/MatrixFactorization.hs
+++ src/MatrixFactorization.hs
@@ -13,7 +13,7 @@ import qualified Bias as B
 
 import Utils
 
-type Matrices = U.Vector Double -- fitted factor matrices P, Q^t stored in row-major order
+type Matrices = U.Vector Double -- factor matrices P, Q^t stored in row-major order
 
 
 {- notes / TODO
@@ -22,8 +22,11 @@ user Reader to encode config stuff like Matrices, BiasModel etc
 
 -}
 
-alpha = 0.01 -- initial learning rate
+
+rate = 0.01 -- initial learning rate
+alpha = 40 -- confidence scaling factor, see Hu et al section 4
 lambda = 0.01 -- Tikhonov regularization paramater
+
 nr_iter = 2
 nFeatures = 10
 nUsers = 943
@@ -31,10 +34,12 @@ nItems = 1682
 fRange = [0..nFeatures-1]
 datasetSize = 80000
 
--- | computes a prediction for rating of user u on item i based on the latent factor vectors
-predict :: Matrices -> BiasModel -> User -> Item -> Rating
-predict para biasModel user item = bias + sum [(para U.! (itemIndex item f)) * (para U.! (userIndex user f))| f <- fRange]
-                      where bias = B.predict biasModel user item
+-- | computes a prediction for the rating of user u on item i 
+predict :: BiasModel -> Matrices -> User -> Item -> Rating
+predict biasModel matrices user item = let
+  bias = B.predict biasModel user item
+  prediction = sum [(matrices U.! (itemIndex item f)) * (matrices U.! (userIndex user f))| f <- fRange]
+  in bias + prediction
 
 -- retrieve the index of an latent item feature in the parameter vector
 itemIndex :: Item -> Int -> Int 
@@ -49,26 +54,26 @@ userIndex x f = nItems + ((x-1) * nFeatures) + f
 -- | computes the latent factor vectors for matrix factorixation
 model :: DataSet -> IO Matrices
 model dataset = do 
-    let n = (nFeatures * nUsers) + (nFeatures * nItems)
-    let matrices = U.replicate n (0.1::Double)
-    D.withVect (V.toList dataset) (runSGD matrices dataset)
+  let n = (nFeatures * nUsers) + (nFeatures * nItems)
+  let matrices = U.replicate n (0.1::Double)
+  D.withVect (V.toList dataset) (runSGD matrices dataset)
 
 
 -- | SGD optimizer
 runSGD :: Matrices -> DataSet -> D.Dataset DataPoint -> IO Matrices
 runSGD matrices all d = do
-  let sgdArgs = S.sgdArgsDefault { S.iterNum = nr_iter, S.gain0=alpha }
+  let sgdArgs = S.sgdArgsDefault { S.iterNum = nr_iter, S.gain0=rate }
   let bm = B.model all
   S.sgd sgdArgs (notify bm all) (grad bm) d matrices 
 
--- Notification run by the SGD function every parameters update.
+-- notification run by the SGD function 
 notify :: BiasModel -> DataSet -> Matrices -> Int -> IO ()
 notify biasModel all matrices k = putStr ("\n" ++
                                           (show k) ++
                                           ("\t") ++
                                           (show (goal biasModel matrices all)))
 
--- cost function returning the squared sum of the errors.
+-- cost function returning the squared sum of the errors
 goal :: BiasModel -> Matrices -> DataSet -> Error
 goal biasModel matrices all = V.sum (V.map (\x -> err2 x) all)
   where err2 (u, i, r) = (r - mu - (sum [el i u f | f <- fRange]))^2
@@ -87,7 +92,7 @@ gradP biasModel matrices dataPoint = do
   f <- fRange
   let (user,item,rating) = dataPoint
       err = errorAt biasModel matrices dataPoint
-      update = err * (matrices U.! (userIndex user f)) - lambda * (matrices U.! (itemIndex user f))
+      update = err * (matrices U.! (userIndex user f)) - lambda * (matrices U.! (itemIndex item f))
   return (itemIndex item f, update)
 
 -- computes the gradient step for the user component
@@ -98,7 +103,8 @@ gradQ biasModel matrices dataPoint = do
       err = errorAt biasModel matrices dataPoint
       update = err * (matrices U.! (itemIndex item f)) - lambda * (matrices U.! (userIndex user f))
   return (userIndex user f, update)
-  
+
+
 -- calculates error for user, item, rating with a given latent factor vector
 errorAt :: BiasModel -> Matrices -> DataPoint -> Error 
 errorAt biasModel matrices (user,item,rating) = rating - bias - innerProd
