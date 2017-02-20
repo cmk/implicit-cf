07e6c80b0a7a76bb0a0e909a899c93e201057f51
chris mckinlay <cem3394@chriss-MacBook.att.net>
2017-02-20 12:39:46 -0800

    cleaning up

--- src/Main.hs
+++ src/Main.hs
@@ -28,12 +28,42 @@ import qualified Data.MultiMap as MM
 
 
 main :: IO ()
---main = execute "knn" "data/test3.csv"
---main = test "knn" "data/trx1.aa" "data/trx1.ab"
-main = test "knn" "data/trx_data.csv" "data/recommend_1.csv"
+--main = execute kNearestNeighbors "data/trx1.aa" "data/trx1.ab" 
+--main = execute kNearestNeighbors "data/trx_data.csv" "data/trx1.ab" 
+main = execute score "data/recommend_1-ALSshort.csv" "data/trx1.ab"
+--main = execute score "data/recommend_1-KNN.csv" "data/trx1.ab"
+--main = test "data/trx_data.csv" "data/recommend_1.csv"
 
---main = convert "data/trx_data.csv" "data/trx_unbatched.csv"
+--main = convert "data/trx1train.csv" "data/trx1_unbatched.csv"
 
+
+
+{-
+execute :: String -> FilePath -> IO ()
+execute algorithm filePath = do
+  input <- readFile filePath
+  let csv = parseCSV filePath input
+  either print print csv
+  --either print (dispatch' algorithm) csv
+
+check csv = print $ head $ countVectorizer csv
+
+
+
+-}
+
+
+execute :: ([Record] -> [Record] -> IO ()) -> FilePath -> FilePath -> IO ()
+execute function trainFile testFile = do
+  trainData <- readFile trainFile
+  testData <- readFile testFile
+  let trainCsv = parseCSV trainFile trainData
+      testCsv = parseCSV testFile testData
+      allCsv = liftM2 (,) trainCsv testCsv
+  either print (uncurry function) allCsv
+
+
+convert :: FilePath -> FilePath -> IO ()
 convert inFilePath outFilePath = let
   header = ["CustomerID", "StockCode", "Quantity"]
   transform csv = writeFile outFilePath $ printCSV
@@ -46,35 +76,11 @@ convert inFilePath outFilePath = let
     let csv = parseCSV inFilePath input
     either print transform csv
 
-
-execute :: String -> FilePath -> IO ()
-execute algorithm filePath = do
-  input <- readFile filePath
-  let csv = parseCSV filePath input
-  either print print csv
-  --either print (dispatch' algorithm) csv
-
-check csv = print $ head $ countVectorizer csv
-
-test :: String -> FilePath -> FilePath -> IO ()
-test algorithm trainFile testFile = do
-  trainData <- readFile trainFile
-  testData <- readFile testFile
-  let trainCsv = parseCSV trainFile trainData
-      testCsv = parseCSV testFile testData
-      allCsv = liftM2 (,) trainCsv testCsv
-  either print (uncurry kNearestNeighbors) allCsv
-
-
-dispatch' :: String -> [Record] -> IO ()
---dispatch' "knn" = kNearestNeighbors
-dispatch' "mf" = matrixFactorization
-
-
+    
 kNearestNeighbors :: [Record] -> [Record] -> IO ()
-kNearestNeighbors trainCsv testCsv = let
-  trainData = map KN.avg $ countVectorizer trainCsv
-  testData = map (read . head) $ tail $ init testCsv :: [User]
+kNearestNeighbors train test = let
+  trainData = map KN.avg $ countVectorizer train      ----MOVE TO KNN FILE!!
+  testData = map (read . head) $ tail $ init test :: [User] --works on holdout trx data and test data
   --users = map fst testData
   header = ["customerId", "recommendedProducts"]
   recs = KN.recommend trainData testData
@@ -83,25 +89,28 @@ kNearestNeighbors trainCsv testCsv = let
   out' = zipWith glue testData out
   in writeFile "data/recommend_1-KNN.csv" $  printCSV $ header:out'
   
-testKNearestNeighbors :: [Record] -> [Record] -> IO ()
-testKNearestNeighbors trainCsv testCsv = let
-  trainData = map KN.avg $ countVectorizer trainCsv
-  testData = map parseToTuple $ init testCsv
-  users = map fst testData
-  userTx = map snd testData
-  recs = KN.recommend trainData users
+
+
+score :: [Record] -> [Record] -> IO ()
+score recommended purchased = let
+  purchases = map parseToTuple $ tail $ init purchased    --MOVE HEAD , TAIL OUT OF countV
+  recs = (map . map) fst $ map M.toList
+                         $ map snd 
+                         $ map parseToTuple
+                         $ tail $ init recommended
+  --users = map fst purchases
+  userTx = map snd purchases
   in do 
-    putStrLn $ "score: " ++ (show $ score userTx recs)
+    putStrLn $ "score: " ++ (show $ scoreList userTx recs)
     putStrLn $ "nrecs: " ++ (show $ length recs)
-
-
-score :: [ItemRatingMap] -> [[Item]] -> Int
-score userTx recs = let
+  
+scoreList :: [ItemRatingMap] -> [[Item]] -> Int
+scoreList userTx recs = let
   items = zip userTx recs
   in sum $ map (uncurry scoreOne) items
   
 scoreOne :: ItemRatingMap -> [Item] -> Int
-scoreOne purchases items | trace (show $ length items) False = undefined
+--scoreOne purchases items | trace (show $ length items) False = undefined
 scoreOne purchases items = let
   out = or [ M.member i purchases | i <- items]
   in case out of
@@ -109,46 +118,6 @@ scoreOne purchases items = let
    False -> 0
 
 
-{-
-
-scoreOne purchases items | trace (show items) False = undefined
-scoreOne purchases items = let
-  r = map fst $ M.toList purchases
-  out = [1.0 | i <- r, elem i items]
-  n = fromIntegral $ length r :: Double
-  in sum out / n
-
-kNearestNeighbors :: [Record] -> IO ()
-kNearestNeighbors csv = let
-  trainData = map KN.avg $ countVectorizer csv
-  testData = [0..11]
-  recs = KN.recommend trainData testData
-  --r = KN.ratings $ KN.findUser (snd $ head closests) users
-  in do
-    putStrLn "Start knn-based recommender"
-    putStrLn $ intercalate "\n" $ map show recs
--}
-
-    
-matrixFactorization :: [Record] -> IO ()
-matrixFactorization csv = do
-  putStrLn "Start factorization-based recommender"
-  let users = countVectorizer csv
-      trainData = dataSet users
-      testData = [0..11]
-      --bm = B.model trainData
-  --(trainData, testData) <- loadData base test
-  putStrLn $ "datasetSize: " ++ (show $ V.length trainData)
-  putStrLn $ "nUsers, nItems " ++ (show $ getParams trainData)
-  m <- MF.model trainData
-  putStrLn ""
-  --print m
-  putStrLn $ MF.showMat $ MF.getR m
-  putStrLn $ intercalate "\n" $ map show $ MF.recommend m testData
-  --putStrLn $ MF.showMat $ MF.getP m
-  --putStrLn $ MF.showMat $ MF.getQ m
-  
-  --putStrLn $ "Mean Absolute Error: " ++ (show $ mae (MF.predict bm m) testData)  
 
 getParams :: DataSet -> (Int, Int)
 getParams dataSet = let
@@ -172,7 +141,8 @@ dataSet users = let
   
 countVectorizer :: [Record] -> [UserRatings]
 countVectorizer csv = let
-  purchases = map parseToTuple $ tail $ init csv --stripping header and trailing line
+  purchases = map parseToTuple $ tail
+                               $ init csv --stripping header and trailing line
   in M.toList $ M.fromListWith (M.unionWith (+)) purchases
   
 
@@ -186,8 +156,60 @@ parseToTuple record = let
   in (name, items')     --FIX THIS IS STUPD
 
 
+testKNearestNeighbors :: [Record] -> [Record] -> IO ()
+testKNearestNeighbors trainCsv testCsv = let
+  trainData = map KN.avg $ countVectorizer trainCsv
+  testData = map parseToTuple $ init testCsv
+  users = map fst testData
+  userTx = map snd testData
+  recs = KN.recommend trainData users
+  in do 
+    putStrLn $ "score: " ++ (show $ scoreList userTx recs)
+    putStrLn $ "nrecs: " ++ (show $ length recs)
+    
+
+matrixFactorization :: [Record] -> IO ()
+matrixFactorization csv = do
+  putStrLn "Start factorization-based recommender"
+  let users = countVectorizer csv
+      trainData = dataSet users
+      testData = [0..11]
+      --bm = B.model trainData
+  --(trainData, testData) <- loadData base test
+  putStrLn $ "datasetSize: " ++ (show $ V.length trainData)
+  putStrLn $ "nUsers, nItems " ++ (show $ getParams trainData)
+  m <- MF.model trainData
+  putStrLn ""
+  --print m
+  putStrLn $ MF.showMat $ MF.getR m
+  putStrLn $ intercalate "\n" $ map show $ MF.recommend m testData
+  --putStrLn $ MF.showMat $ MF.getP m
+  --putStrLn $ MF.showMat $ MF.getQ m
+  
+  --putStrLn $ "Mean Absolute Error: " ++ (show $ mae (MF.predict bm m) testData)  
+
+{-
+
+scoreOne purchases items | trace (show items) False = undefined
+scoreOne purchases items = let
+  r = map fst $ M.toList purchases
+  out = [1.0 | i <- r, elem i items]
+  n = fromIntegral $ length r :: Double
+  in sum out / n
+
+kNearestNeighbors :: [Record] -> IO ()
+kNearestNeighbors csv = let
+  trainData = map KN.avg $ countVectorizer csv
+  testData = [0..11]
+  recs = KN.recommend trainData testData
+  --r = KN.ratings $ KN.findUser (snd $ head closests) users
+  in do
+    putStrLn "Start knn-based recommender"
+    putStrLn $ intercalate "\n" $ map show recs
+-}
 
 
+    
 {-
 
 import Data.Csv
